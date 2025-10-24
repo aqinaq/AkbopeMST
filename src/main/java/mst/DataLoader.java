61256cfca4bc57898cf137bb26e7d67201e98f73
asuran <bopebaqytkeldy@gmail.com>
2025-10-25 04:25:28 +0500

    they do not appeared on github main branch and i cannot merge the branches(жоғалып кете береді) so i committed all of them at once

--- src/main/java/mst/DataLoader.java
+++ src/main/java/mst/DataLoader.java
@@ -1,4 +1,46 @@
 package mst;
+import com.google.gson.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class DataLoader {
+
+    public static List<Graph> loadGraphs(String filePath) throws IOException {
+        try (Reader reader = new InputStreamReader(
+                new FileInputStream(filePath), StandardCharsets.UTF_8)) {
+
+            JsonObject root = JsonParser.parseReader(reader).getAsJsonObject();
+            JsonArray graphsArray = root.getAsJsonArray("graphs");
+            List<Graph> graphs = new ArrayList<>();
+
+            for (JsonElement graphElem : graphsArray) {
+                JsonObject gObj = graphElem.getAsJsonObject();
+                JsonArray nodesArr = gObj.getAsJsonArray("nodes");
+                JsonArray edgesArr = gObj.getAsJsonArray("edges");
+
+                Map<String, Integer> nameToIndex = new LinkedHashMap<>();
+                Map<Integer, String> indexToName = new LinkedHashMap<>();
+                for (int i = 0; i < nodesArr.size(); i++) {
+                    String name = nodesArr.get(i).getAsString();
+                    nameToIndex.put(name, i);
+                    indexToName.put(i, name);
+                }
+
+                Graph graph = new Graph(nodesArr.size());
+                for (JsonElement eElem : edgesArr) {
+                    JsonObject e = eElem.getAsJsonObject();
+                    String from = e.get("from").getAsString();
+                    String to = e.get("to").getAsString();
+                    int w = e.get("weight").getAsInt();
+                    graph.addEdge(nameToIndex.get(from), nameToIndex.get(to), w);
+                }
+
+                graph.setNameMap(nameToIndex, indexToName);
+                graph.setId(gObj.get("id").getAsInt());
+                graphs.add(graph);
+            }
+            return graphs;
+        }
+    }
 }
--- src/main/java/mst/KruskalAlgorithm.java
+++ src/main/java/mst/KruskalAlgorithm.java
@@ -1,4 +1,94 @@
 package mst;
+import java.util.*;
 
 public class KruskalAlgorithm {
+
+    private static class DSU {
+        int[] parent, rank;
+        int findOps = 0;
+        int unionOps = 0;
+
+        DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) parent[i] = i;
+        }
+
+        int find(int x) {
+            findOps++;
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        boolean union(int a, int b) {
+            unionOps++;
+            int ra = find(a);
+            int rb = find(b);
+            if (ra == rb) return false;
+            if (rank[ra] < rank[rb]) {
+                parent[ra] = rb;
+            } else if (rank[ra] > rank[rb]) {
+                parent[rb] = ra;
+            } else {
+                parent[rb] = ra;
+                rank[ra]++;
+            }
+            return true;
+        }
+    }
+
+    public static MSTResult runKruskal(Graph graph) {
+        int REPEAT = 1000;
+        long totalNs = 0;
+
+        MSTResult firstResult = runOnce(graph);
+
+        for (int i = 0; i < REPEAT; i++) {
+            long start = System.nanoTime();
+            runOnce(graph);
+            long end = System.nanoTime();
+            totalNs += (end - start);
+        }
+
+        double avgNs = (double) totalNs / REPEAT;
+
+        return new MSTResult("Kruskal", firstResult.getMstEdges(), firstResult.getTotalCost(),
+                firstResult.getVertexCount(), firstResult.getEdgeCount(),
+                firstResult.getOperations(), (long) avgNs);
+    }
+
+    private static MSTResult runOnce(Graph graph) {
+        int V = graph.getVertices();
+        List<Edge> edges = new ArrayList<>(graph.getEdges());
+        Collections.sort(edges);
+
+        DSU dsu = new DSU(V);
+        List<Edge> mstEdges = new ArrayList<>();
+        int totalCost = 0;
+        int comparisons = 0;
+
+        for (Edge e : edges) {
+            comparisons++;
+            int u = e.getSrc();
+            int v = e.getDest();
+
+            if (dsu.find(u) != dsu.find(v)) {
+                if (dsu.union(u, v)) {
+                    mstEdges.add(e);
+                    totalCost += e.getWeight();
+                }
+            }
+
+            if (mstEdges.size() == V - 1)
+                break;
+        }
+
+        Map<String, Integer> ops = new HashMap<>();
+        ops.put("comparisons", comparisons);
+        ops.put("findOps", dsu.findOps);
+        ops.put("unionOps", dsu.unionOps);
+
+        return new MSTResult("Kruskal", mstEdges, totalCost, V, graph.getEdges().size(), ops, 0);
+    }
 }
