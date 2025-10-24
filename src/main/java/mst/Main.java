552707e36c26a985820b6267b53fe11560c7edab
asuran <bopebaqytkeldy@gmail.com>
2025-10-25 02:17:37 +0500

    integrate algorithms and process datasets runs Prim and Kruskal on all inputs records MST results to output JSON

--- src/main/java/mst/Main.java
+++ src/main/java/mst/Main.java
@@ -1,17 +1,64 @@
 package mst;
+import com.google.gson.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws Exception {
+
+        String[] inputFiles = {
+                "src/main/resources/input_small.json",
+                "src/main/resources/input_medium.json",
+                "src/main/resources/input_large.json",
+                "src/main/resources/input_extralarge.json"
+        };
+
+        for (String inputFile : inputFiles) {
+            String outputFile = inputFile.replace("input", "output");
+
+            System.out.println("Processing dataset: " + inputFile);
+
+            List<Graph> graphs = DataLoader.loadGraphs(inputFile);
+            JsonArray results = new JsonArray();
+
+            JsonObject root = new JsonObject();
+            root.add("results", results);
+
+            try (Writer writer = new FileWriter(outputFile)) {
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                gson.toJson(root, writer);
+            }
+
+            System.out.println("Results written to: " + outputFile + "\n");
         }
+
+        System.out.println("datasets processed successfully!");
+    }
+
+    private static JsonObject buildAlgorithmJson(Graph g, MSTResult result) {
+        JsonObject algoJson = new JsonObject();
+
+        JsonArray edgeArray = new JsonArray();
+        for (Edge e : result.getMstEdges()) {
+            JsonObject edgeJson = new JsonObject();
+            edgeJson.addProperty("from", g.getIndexToName().get(e.getSrc()));
+            edgeJson.addProperty("to", g.getIndexToName().get(e.getDest()));
+            edgeJson.addProperty("weight", e.getWeight());
+            edgeArray.add(edgeJson);
+        }
+        algoJson.add("mst_edges", edgeArray);
+
+        algoJson.addProperty("total_cost", result.getTotalCost());
+
+        JsonObject opsJson = new JsonObject();
+        result.getOperations().forEach(opsJson::addProperty);
+        algoJson.add("operations", opsJson);
+
+        algoJson.addProperty("execution_time_ns", Math.round(result.getTimeNs() * 100.0) / 100.0);
+
+        return algoJson;
     }
 }
